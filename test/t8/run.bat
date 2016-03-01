1a679d24fda24af69a8484f96cbf43d242af1d2f
daniel.whitehouse <daniel.whitehouse@D8Z7HX12.CRB.LOCAL>
2016-03-01 08:48:24 -0500

    0.0.3

---
+++ test/t8/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java -jar %~dp0tester.jar %*
+pause >nul
---
+++ test/testerRun.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+start %~dp0%/t1/run.bat
+start %~dp0%/t2/run.bat
+start %~dp0%/t3/run.bat
+start %~dp0%/t4/run.bat
+start %~dp0%/t5/run.bat
+start %~dp0%/t6/run.bat
+start %~dp0%/t7/run.bat
+start %~dp0%/t8/run.bat
+start %~dp0%/t9/run.bat
+start %~dp0%/t10/run.bat
